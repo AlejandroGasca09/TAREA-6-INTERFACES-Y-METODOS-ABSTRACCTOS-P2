95eee8a4aa9949dcec1fd2790a5428b2594c4313
AlejandroGasca09 <119777270+AlejandroGasca09@users.noreply.github.com>
2023-04-24 03:08:53 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        AlumnoDeportistaArtista ada = new AlumnoDeportistaArtista();
+        ada.setNombre("Juan");
+        ada.setArte("pintura");
+        ada.setDeporte("basket ball");
+        System.out.println(ada);
+        ada.ensayar("Oleo");
+        ada.entrenar();
+        ada.presentarCompetencia("Esta compitiendo en la Fes ");
     }
 }
